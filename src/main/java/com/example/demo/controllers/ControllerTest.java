06569bf53c4d9d4d3f1d1a6804ed4ada97213294
Никита <nicitazaletin@yandex.ru>
2021-05-06 00:44:53 +0300

    Работает отправка изображений

--- src/main/java/com/example/demo/controllers/ControllerTest.java
+++ src/main/java/com/example/demo/controllers/ControllerTest.java
@@ -1,16 +1,43 @@
 package com.example.demo.controllers;
+import com.example.demo.User;
+import com.example.demo.models.Menu;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @RestController
+@CrossOrigin
 public class ControllerTest {
+    private String BASE_URL = "http://localhost:8080/menu/";
 
     @GetMapping("/menu")
-    public ResponseEntity<String> testMeth() {
-        String a = "It's work";
-        return new ResponseEntity<>(a,HttpStatus.OK);
+    public Menu getMenu(){
+        Menu menu = new Menu();
+        menu.setTitle("Загаловок");
+        menu.setDescription("Описание");
+        menu.setImg(BASE_URL + "1");
+        return menu;
+    }
+
+
+    @GetMapping("/menu/{id}")
+    @ResponseBody
+    public ResponseEntity<byte[]> getPhoto(@PathVariable("id") String id) throws IOException {
+
+        File imgPath = new File("C:\\Users\\Nikita\\Desktop\\images\\test_" + id + ".jpg" );
+
+        byte[] image = Files.readAllBytes(imgPath.toPath());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setContentLength(image.length);
+        return new ResponseEntity<>(image, headers, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/example/demo/models/Menu.java
@@ -0,0 +1,32 @@
+package com.example.demo.models;
+
+
+public class Menu {
+    private String title;
+    private String description;
+    private String img;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
