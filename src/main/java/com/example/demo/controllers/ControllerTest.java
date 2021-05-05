813327d657d37faa93a2e5539a816b583173e769
Никита <nicitazaletin@yandex.ru>
2021-05-05 20:52:54 +0300

    Сервер может работать с GET-запросами

---
+++ src/main/java/com/example/demo/controllers/ControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.controllers;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerTest {
+
+    @GetMapping("/menu")
+    public ResponseEntity<String> testMeth() {
+        String a = "It's work";
+        return new ResponseEntity<>(a,HttpStatus.OK);
+    }
+}
